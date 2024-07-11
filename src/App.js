bad4e2749de5260c04acd6543a44202633a2594c
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-11 16:46:35 +0100

    fix prop drilling

--- src/App.js
+++ src/App.js
@@ -55,17 +55,24 @@ export default function App() {
 
   return (
     <>
-      <NavBar />
-      <Main movies={movies} />
+      <NavBar>
+        <Search />
+        <Numresults movies={movies} />
+      </NavBar>
+      <Main>
+        <ListBox>
+          <MovieList movies={movies} />
+        </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
-function NavBar() {
+function NavBar({ children }) {
   return (
     <nav className="nav-bar">
       <Logo />
-      <Search />
-      <Numresults />
+      {children}
     </nav>
   );
 }
@@ -89,22 +96,18 @@ function Search() {
     />
   );
 }
-function Numresults() {
+function Numresults({ movies }) {
   return (
     <p className="num-results">
-      Found <strong>x</strong> results
+      Found <strong>{movies.length}</strong> results
     </p>
   );
 }
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
-function ListBox({ movies }) {
+
+function ListBox({ children }) {
   const [watched, setWatched] = useState(tempWatchedData);
 
   const [isOpen1, setIsOpen1] = useState(true);
@@ -116,7 +119,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && children}
     </div>
   );
 }
